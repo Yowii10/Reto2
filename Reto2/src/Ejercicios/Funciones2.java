32cc0413955074b249c00661a9d40d4559bb1643
Carlos809-stack <carloscalvobernal@gmail.com>
2024-10-31 08:47:27 +0100

    creadas funciones de temperatura

--- Reto2/src/Ejercicios/Funciones2.java
+++ Reto2/src/Ejercicios/Funciones2.java
@@ -1,5 +1,14 @@
 package Ejercicios;
 
 public class Funciones2 {
-
+	
+public static double celsiusaFahrentheir(double grados) {
+	return grados*9/5+32;
+			
+		}
+public static double fahrentheiraCelsius(double grados) {
+	return grados-32*5/9;
+		
+	}
+		
 }
