d5bc2b911b8f2fc6741dd82b9e76494ffae63f20
Carlos809-stack <carloscalvobernal@gmail.com>
2024-10-31 09:38:25 +0100

    He creado los dos submenus

--- Reto2/src/Ejercicios/Funciones2.java
+++ Reto2/src/Ejercicios/Funciones2.java
@@ -1,8 +1,50 @@
 package Ejercicios;
 
+import java.util.Scanner;
+
 public class Funciones2 {
-	
-public static double celsiusaFahrentheir(double grados) {
+	public static void submenuTemperatura(Scanner sc) {
+		int opcion=0;
+		do {
+		System.out.println("Selecciona la conversion de longitud");
+		System.out.println("1.Celsius a Fahrentheit");
+		System.out.println("2.Fahrentheit a Celsius");
+		System.out.println("0.Salir");
+
+		int numero= Ej25_22.dimeEntero("opcion", sc);
+		switch(numero){
+		case 1:
+			double celsius = Ej25_22.dimeDouble("Escribe el numero de grados celsius", sc);
+			System.out.println(celsiusaFahrentheit(celsius) + "ºF"); 
+			break;
+		case 2:
+			double fahrenheit = Ej25_22.dimeDouble("Escribe el numero de grados fahrenheit", sc);
+			System.out.println(fahrentheiraCelsius(fahrenheit) + "ºC"); 
+			break;
+		}
+		}while(opcion!=0);
+	}
+	public static void submenuVolumen(Scanner sc) {
+		int opcion=0;
+		do {
+		System.out.println("Selecciona la conversion de longitud");
+		System.out.println("1.Litros a Galones");
+		System.out.println("2.Galones a Litros");
+		System.out.println("0.salir");
+		int numero= Ej25_22.dimeEntero("opcion", sc);
+		switch(numero){
+		case 1:
+			double litros = Ej25_22.dimeDouble("Escribe el numero de litros", sc);
+			System.out.println(litrosaGalones(litros) + "L"); 
+			break;
+		case 2:
+			double galones = Ej25_22.dimeDouble("Escribe el numero de galones", sc);
+			System.out.println(galonesaLitros(galones) + "G"); 
+			break;
+		}
+		}while(opcion!=0);
+	}
+public static double celsiusaFahrentheit(double grados) {
 	return grados*9/5+32;
 			
 		}
