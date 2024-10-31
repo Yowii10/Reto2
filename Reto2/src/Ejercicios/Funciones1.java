950172243e29e96766421285299c62415fc86e26
Yowii10 <yoeliranzo@gmail.com>
2024-10-31 08:46:25 +0100

    Creadas funciones longitud

--- Reto2/src/Ejercicios/Funciones1.java
+++ Reto2/src/Ejercicios/Funciones1.java
@@ -1,5 +1,14 @@
 package Ejercicios;
 
+
 public class Funciones1 {
+	
+	public static double millasKM(double millas) {
+		return millas * 1.609; 
+	}
+	public static double kmMillas(double km) {
+		return km * 0.62137119; 
+	}
+
 
 }
