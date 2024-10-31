7d51c0a5dda98cb8ec46988274411cac357a621a
Yowii10 <yoeliranzo@gmail.com>
2024-10-31 08:52:09 +0100

    creada funciones peso

--- Reto2/src/Ejercicios/Funciones1.java
+++ Reto2/src/Ejercicios/Funciones1.java
@@ -9,6 +9,13 @@ public class Funciones1 {
 	public static double kmMillas(double km) {
 		return km * 0.62137119; 
 	}
-
+	
+	public static double KgLibra (double kg) {
+		return kg * 2.20462262;
+	}
+	public static double libraKg(double libra) {
+		return libra * 0.45359237;
+	}
+	
 
 }
