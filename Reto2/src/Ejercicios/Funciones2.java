60f947f83c8d8f59be395361ce8badd915564ef4
Carlos809-stack <carloscalvobernal@gmail.com>
2024-10-31 08:51:29 +0100

    Creada las funciones de volumen

--- Reto2/src/Ejercicios/Funciones2.java
+++ Reto2/src/Ejercicios/Funciones2.java
@@ -10,5 +10,13 @@ public static double fahrentheiraCelsius(double grados) {
 	return grados-32*5/9;
 		
 	}
+public static double litrosaGalones(double litros) {
+	return litros*0.264172;
+		
+	}
+public static double galonesaLitros(double galones) {
+	return galones*3.785;
+		
+	}
 		
 }
